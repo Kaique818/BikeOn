6510d131e8e7d868d2a93b911c8fef7be2731c83
kaique818 <kaiquematheus142005@gmail.com>
2024-10-10 20:02:41 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etec/myapp/BikesDoacaoFragment.java
@@ -0,0 +1,22 @@
+package br.com.etec.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class BikesDoacaoFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_bikes_doacao, container, false);
+
+        return view;
+    }
+}
